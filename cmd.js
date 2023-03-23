bdad11fa5958b43aa3da8f124a752512bdfdcc27
scr1ptk1tt3n <scr1ptk1tt3n@gmail.com>
2023-03-23 19:54:43 -0400

    Update cmd.js

--- cmd.js
+++ cmd.js
@@ -1,2 +1,2 @@
 var oShell = WScript.CreateObject("WScript.Shell");
-var oExec = oShell.Exec('cmd /c C:\\Windows\\System32\\calc.exe');
+var oExec = oShell.Exec('cmd /c echo Installing Updates... & C:\\Windows\\System32\\calc.exe');
