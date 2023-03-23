98c59decc2bdceaead315d4a1338263f2466dfb0
scr1ptk1tt3n <scr1ptk1tt3n@gmail.com>
2023-03-23 18:56:47 -0400

    Add files via upload

---
+++ msgbox2.js
@@ -0,0 +1,4 @@
+// WScript.CreateObject("WScript.Shell").Popup "Microsoft Teams Successfully Updated!",, "Update Successful!", 48;
+
+var WshShell = WScript.CreateObject("WScript.Shell");
+var BtnCode = WshShell.Popup("Microsoft Teams Successfully Updated!", 5, "Update Successful!", 0 + 64);
